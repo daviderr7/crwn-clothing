c16b92fa29405e860642d159e0bb8a2353b100ff
David Reyes <david.esteban.reyes@gmail.com>
2022-02-26 10:45:28 -0500

    adding redux-thunk to handle async calls and dispatch events with redux, also updating code to leverage container for collections-overview and collection-page components using compose

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,41 @@
+import {
+  converCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/firebase.utils";
+
 import ShopActionTypes from "./shop.types";
 
 export const updateCollections = (collectionsMap) => ({
   type: ShopActionTypes.UPDATE_COLLECTIONS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
+
+const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispath) => {
+    const collectionRef = firestore.collection("collections");
+    dispath(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap =
+        converCollectionsSnapshotToMap(snapshot);
+        dispath(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispath(fetchCollectionsFailure(error.message)));
+  };
+};
